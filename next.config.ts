5f6f774026ec2aed4747fd25404616556ae1f6ea
jaey8den <89349331+jaey8den@users.noreply.github.com>
2025-08-29 20:10:45 +0800

    config and package update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: { unoptimized: true },
   basePath: "/jaey8den-portfolio",
+  assetPrefix: "/jaey8den-portfolio/",
 };
 
 export default nextConfig;
