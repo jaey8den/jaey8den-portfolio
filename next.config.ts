e3b812e6670d01fdbf6469b96476f94caa56a5e9
jaey8den <89349331+jaey8den@users.noreply.github.com>
2025-08-29 20:04:56 +0800

    config and package change

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  base: "/jaey8den-portfolio/",
+  basePath: "/jaey8den-portfolio",
 };
 
 export default nextConfig;
