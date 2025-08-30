59bef8f9adc2a65822e21587d10be7e7b2f24c2e
jaey8den <89349331+jaey8den@users.noreply.github.com>
2025-08-30 17:45:56 +0800

    updating config for custom domain

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   images: { unoptimized: true },
-  basePath: "/jaey8den-portfolio",
-  assetPrefix: "/jaey8den-portfolio/",
+  /* For none custom domain */
+  // basePath: "/jaey8den-portfolio",
+  // assetPrefix: "/jaey8den-portfolio/",
+  /* For custom domain */
+  basePath: "",
+  assetPrefix: "/",
 };
 
 export default nextConfig;
