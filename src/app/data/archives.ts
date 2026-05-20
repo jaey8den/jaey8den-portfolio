4b3476389f47fee17d54ac2edabc57dfb8781b2e
jaey8den <89349331+jaey8den@users.noreply.github.com>
2026-05-21 00:33:14 +0800

    Spelling error

--- src/app/data/archives.ts
+++ src/app/data/archives.ts
@@ -11,7 +11,8 @@ export const archives: Archive[] = [
     slug: "website",
     title: "Portfolio Website (2025)",
     screenshots: ["/archives/website/web-1.gif", "/archives/website/web-2.gif"],
-    description: "Responsive portfolio website to showcase notable experiences and works.",
+    description:
+      "Responsive portfolio website to showcase notable experiences and works.",
     skills: "NextJS, TailwindCSS",
   },
   {
@@ -33,14 +34,16 @@ export const archives: Archive[] = [
       "/archives/ock/ock-2.png",
       "/archives/ock/ock-3.png",
     ],
-    description: "A revamp of Old Chang Kee's website to give it a modern look.",
+    description:
+      "A revamp of Old Chang Kee's website to give it a modern look.",
     skills: "Web Design, UI/UX, Copywrite, Photoshop",
   },
   {
     slug: "jne",
     title: "JnE (2022)",
     screenshots: ["/archives/jne/jne-1.png", "/archives/jne/jne-2.png"],
-    description: "An online apparel store. Using databases and session variables to keep stock and cart items.",
+    description:
+      "An online apparel store. Using databases and session variables to keep stock and cart items.",
     skills: "php, MySQL, CSS, Javascript",
   },
   {
@@ -51,7 +54,8 @@ export const archives: Archive[] = [
       "/archives/jio/jio-2.png",
       "/archives/jio/jio-3.png",
     ],
-    description: "An productivity app connecting users virtually in a game-like environment, posed as cats.",
+    description:
+      "A productivity app connecting users virtually in a game-like environment, posed as cats.",
     skills: "Unity3D, EC2, Photoshop, C#, MySQL",
   },
 ];
